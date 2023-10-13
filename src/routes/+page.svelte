0981fc49d37f423a2aecd7a0a66ee29147103510
Hawa <sandyhawa6@gmail.com>
2023-10-13 11:45:41 +0000

    second commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,89 @@
+<script>
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from 'flowbite-svelte';
+    import { SearchOutline } from 'flowbite-svelte-icons';
+   
+    import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper, SidebarDropdownItem, SidebarDropdownWrapper } from 'flowbite-svelte';
+    import { ChartPieSolid, GridSolid, ShoppingCartSolid, MailBoxSolid, UserSolid, ArrowRightToBracketSolid, FileEditSolid } from 'flowbite-svelte-icons';
+    let spanClass = 'flex-1 ml-3 whitespace-nowrap';
 
-<div class="bg-gray-900 py-24 sm:py-32">
-    <div class="mx-auto max-w-7xl px-6 text-center lg:px-8">
-      <div class="mx-auto max-w-2xl">
-        <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Meet our team</h2>
-        <p class="mt-4 text-lg leading-8 text-gray-400">We’re a dynamic group of individuals who are passionate about what we do.</p>
+  </script>
+  
+  <Navbar>
+    <NavBrand href="/">
+      <img src="/images/flowbite-svelte-icon-logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    </NavBrand>
+    <div class="flex md:order-2">
+      <Button color="none" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
+        <SearchOutline class="w-5 h-5 mr-50" />
+      </Button>
+      <div class="hidden relative md:block">
+        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+          <SearchOutline class="w-4 h-4" />
+        </div>
+        <Input id="search-navbar" class="pl-10 " placeholder="Search..." />
       </div>
-      <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8">
-        <li class="rounded-2xl bg-gray-800 px-8 py-10">
-          <img class="mx-auto h-48 w-48 rounded-full md:h-56 md:w-56" src="https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="">
-          <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-white">Leonard Krasner</h3>
-          <p class="text-sm leading-6 text-gray-400">Senior Designer</p>
-          <ul role="list" class="mt-6 flex justify-center gap-x-6">
-            <li>
-              <a href="#" class="text-gray-400 hover:text-gray-300">
-                <span class="sr-only">Twitter</span>
-                <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                  <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="text-gray-400 hover:text-gray-300">
-                <span class="sr-only">LinkedIn</span>
-                <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                  <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
-                </svg>
-              </a>
-            </li>
-          </ul>
-        </li>
-  
-        <!-- More people... -->
-      </ul>
+      <Button color="dark">connect wallet</Button>
+      <NavHamburger />
     </div>
-  </div>
+    <NavUl>
+      <NavLi href="/" active={true}>Home</NavLi>
+      <NavLi href="/about">About</NavLi>
+      <NavLi href="/docs/components/navbar">Navbar</NavLi>
+      
+    </NavUl>
+  </Navbar>
+
+
+  <!-- sidebar -->
   
+  <Sidebar>
+    <SidebarWrapper>
+      <SidebarGroup>
+        <SidebarItem label="Dashboard">
+          <svelte:fragment slot="icon">
+            <ChartPieSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+          </svelte:fragment>
+        </SidebarItem>
+        <SidebarDropdownWrapper label="market place">
+          <svelte:fragment slot="icon">
+            <ShoppingCartSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+          </svelte:fragment>
+          <SidebarDropdownItem label="Popular collection" />
+          <SidebarDropdownItem label="Auction" />
+          <SidebarDropdownItem label="VIP" />
+        </SidebarDropdownWrapper>
+        <SidebarItem label="Kanban" {spanClass}>
+          <svelte:fragment slot="icon">
+            <GridSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+          </svelte:fragment>
+          <svelte:fragment slot="subtext">
+            <span class="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300"> Pro </span>
+          </svelte:fragment>
+        </SidebarItem>
+        <SidebarItem label="Inbox" {spanClass}>
+          <svelte:fragment slot="icon">
+            <MailBoxSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+          </svelte:fragment>
+          <svelte:fragment slot="subtext">
+            <span class="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-primary-600 bg-primary-200 rounded-full dark:bg-primary-900 dark:text-primary-200"> 3 </span>
+          </svelte:fragment>
+        </SidebarItem>
+        <SidebarItem label="Users">
+          <svelte:fragment slot="icon">
+            <UserSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+          </svelte:fragment>
+        </SidebarItem>
+        <SidebarItem label="Sign In">
+          <svelte:fragment slot="icon">
+            <ArrowRightToBracketSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+          </svelte:fragment>
+        </SidebarItem>
+        <SidebarItem label="Sign Up">
+          <svelte:fragment slot="icon">
+            <FileEditSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+          </svelte:fragment>
+        </SidebarItem>
+      </SidebarGroup>
+    </SidebarWrapper>
+  </Sidebar>
